558fd68c7220042647aba58954413396acf159b3
Ramazan <rbugday98@gmail.com>
2026-07-28 13:28:31 +0200

    message

--- resources/views/advertisements/show.blade.php
+++ resources/views/advertisements/show.blade.php
@@ -5,13 +5,13 @@
 @section('content')
     <div class="flex h-[80vh] bg-white border rounded-lg overflow-hidden max-w-6xl mx-auto my-6">
         
-        <div class="w-1/3 border-r bg-gray-50 overflow-y-auto">
+        <div class="w-1/3 border-r bg-gray-50 flex flex-col h-full">
             
-            <div class="p-4 font-bold border-b bg-gray-100 flex justify-between items-center">
+            <div class="p-4 font-bold border-b bg-gray-100 flex justify-between items-center shrink-0">
                 <h2 class="font-bold text-base text-gray-900 mb-2">Bids</h2>
             </div>
 
-            <ul class="divide-y">
+            <ul class="divide-y flex-1 overflow-y-auto">
                 @forelse ($advertisement->bids as $bid)
                     <li class="p-4 hover:bg-gray-100 transition bg-white">
                         <div class="flex justify-between items-start">
@@ -32,6 +32,27 @@
                 @endforelse
                 
             </ul>
+
+            @if(Auth::id() !== $advertisement->user_id)
+    <div class="p-4 border-t bg-white shrink-0 sticky bottom-0">     
+        <form action="{{ route('advertisements.bids.store', $advertisement->id) }}" method="POST" class="flex gap-2 items-center">
+            @csrf
+
+            
+            <div class="relative flex-1">
+                <div class="absolute inset-y-0 left-0 pl-3 flex items-center ">
+                    <span class="text-gray-500 sm:text-sm">€</span>
+                </div>
+                <input type="number" name="amount" id="bid_amount" step="0.01" min="1" max="9999" placeholder="Make a Bid..." required
+                    class="w-full border rounded-lg pl-7 pr-4 py-2 text-sm">
+            </div>
+
+            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded" onclick="return confirm('Confirm to make a bid of €' + document.getElementById('bid_amount').value + ' on {{ $advertisement->title }}')">
+                Bid
+            </button>
+        </form>       
+    </div>
+    @endif
         </div>
             
 
@@ -41,7 +62,7 @@
     <div class="container mx-auto px-4 py-8 max-w-4xl">
         @include('partials.advertisement-card')
 
-        @auth
+        
             @if(Auth::id() !== $advertisement->user_id)
                 <h2 class="font-bold text-base text-gray-900 mb-2">Ask the seller a question</h2>
 
@@ -56,27 +77,8 @@
                     </button>
                 </form>
             @endif
-        @else
-            <p class="text-sm text-gray-600">You have to be <a href="{{ route('login')}}" class="text-blue-600 font-semibold hover:text-blue-300">logged in</a>to send a message</p>
-        @endauth
+        
     </div>
 
-    <div class="p-4 border-t bg-white shrink-0">     
-        <form action="{{ route('advertisements.bids.store', $advertisement->id) }}" method="POST" class="flex gap-2 items-center">
-            @csrf
-
-            <div class="relative flex-1">
-                <div class="absolute inset-y-0 left-0 pl-3 flex items-center ">
-                    <span class="text-gray-500 sm:text-sm">€</span>
-                </div>
-                <input type="number" name="amount" id="bid_amount" step="0.01" min="1" max="9999" placeholder="Make a Bid..." required
-                    class="w-full border rounded-lg pl-7 pr-4 py-2 text-sm">
-            </div>
-
-            <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded" onclick="return confirm('Confirm to make a bid of €' + document.getElementById('bid_amount').value + ' on {{ $advertisement->title }}')">
-                Bid
-            </button>
-        </form>       
-    </div>
-
+    
 @endsection
