26aa086867c64536262663db689115e40cdd10f8
Maos-strongest-coder <rbugday98@gmail.com>
2026-07-30 13:59:26 +0200

    inle to css files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
             <meta charset="UTF-8">
                 <title>Mo's Marktplaats - @yield('title')</title>
                 <script src="https://cdn.tailwindcss.com"></script>
+                @vite(['resources/css/app.css'])
         </head>
 
         <body class="h-full">
--- resources/views/partials/advertisement-card.blade.php
+++ resources/views/partials/advertisement-card.blade.php
@@ -1,25 +1,58 @@
-<div class="bg-white rounded-lg overflow-hidden">
+<div class="adv-card">
     <p>category: {{ $advertisement->category->name}}</p>
-
-    <img src="{{asset($advertisement->image)}}" class="w-full rouded-lg">
-
-    <a href="{{ route('advertisements.show', $advertisement->id) }}" class="block mt-2 text-lg font-bold text-gray-900 hover:text-blue-500">{{ $advertisement->title }}</a>
     
-    @if(request()->routeIs('advertisements.show'))
-    <p class="mt-1 text-gray-600">{{ $advertisement->content }}</p>
-    @else
-    <p class="mt-1 text-gray-600">{{ Str::limit($advertisement->content, 100) }}</p>
-    @endif
+    <div class="adv-card-image-wrapper">
+    
+        <img src="{{asset($advertisement->image)}}" class="adv-card-img">
+    
+    </div>
+    
+    <div class="adv-card-body">
 
-    <h3 class="text-lg font-bold">${{ $advertisement->price }}</h3>
+        <a href="{{ route('advertisements.show', $advertisement->id) }}" class="adv-card-title">
+            {{ $advertisement->title }}
+        </a>
+
+        @if(request()->routeIs('advertisements.show'))
+            <p class="adv-card-text">
+                {{ $advertisement->content }}
+            </p>
+        @else
+            <p class="adv-card-text">
+                {{ Str::limit($advertisement->content, 100) }}
+            </p>
+        @endif
+    </div>   
+    
+    <div class="adv-card-footer">
+
+        <span class="adv-card-price">
+            ${{ $advertisement->price }}
+        </span>
+
+        
+        @if (request()->routeIs('advertisements.show', 'advertisements.my') && Auth()->id() === $advertisement->user_id)
+            <a href="{{ route('advertisements.edit', $advertisement->id) }}" class="bg-yellow-500 text-white px-4 py-2 rounded">
+                Edit
+            </a>
+
+            <a>
+                Delete
+            </a>
+        @endif
+    </div>
+    
+        
+    
 
     @if (request()->routeIs('advertisements.show', 'advertisements.my') && Auth()->id() === $advertisement->user_id)
         <div class="p-4 border-t bg-white shrink-0">
             <a href="{{ route('advertisements.edit', $advertisement->id) }}" class="bg-yellow-500 text-white px-4 py-2 rounded">
                 Edit</a>
 
-            <a>
-                Delete</a>
+            <a href="{{ route('advertisements.destroy', $advertisement->id) }}">
+                Delete
+            </a>
         </div>
     @endif
 </div>
